7529f751ec5ba049fe9f159e2adca8a8bf3d5ad8
tifflouie <tiffany.louie2013@gmail.com>
2023-07-23 21:47:00 -0700

    my first c

---
+++ example.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 123;
+
+    printf("Hello World 3 \n");
+
+
+    printf("%i \n", i);
+
+    i = 456;
+    printf("%i \n", i);
+    
+    return 1;
+}
